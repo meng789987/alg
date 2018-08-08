d92047ca30231ccb70d9c38edaf058ab58c14180
unknown <meng789987@gmail.com>
2018-08-08 10:00:49 -0700

    temp

--- codes/src/Solution.cs
+++ codes/src/Solution.cs
@@ -11,9 +11,9 @@ namespace alg
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("BinarySearch:");
-            //new array.BinarySearch().Test();
-            Test();
+            Console.WriteLine("Lc564_Find_the_Closest_Palindrome:");
+            new leetcode.Lc564_Find_the_Closest_Palindrome().Test();
+            //Test();
 
             Console.WriteLine("Press any key to exit ...");
             Console.ReadKey();
@@ -29,103 +29,6 @@ namespace alg
             var ss = new SortedSet<int>();
             Console.WriteLine(ss.Min);
 
-            var x = new Solution().KthSmallestPrimeFraction(new int[] { 1, 2, 3, 5 }, 3);
-            Console.WriteLine(x);
-        }
-        public int[] KthSmallestPrimeFraction(int[] A, int K)
-        {
-            int n = A.Length;
-            var indice = new int[n];
-            for (int i = 0; i < n; i++) indice[i] = i;
-
-            var res = Biselect(A, indice, K, K);
-            return new int[] { (int)res[0][1], (int)res[0][2] };
-        }
-
-        // return lk-th and rk-th elements { p/q, p, q }
-        double[][] Biselect(int[] A, int[] selected, int lk, int rk)
-        {
-            int n = selected.Length;
-            // base case
-            if (n <= 2)
-            {
-                var nums = new double[n * n][];
-                int k = 0;
-                foreach (var i in selected)
-                    foreach (var j in selected)
-                        nums[k++] = MatrixItem(A, i, j);
-                Array.Sort(nums, (a, b) => a[0].CompareTo(b[0]));
-                return new double[][] { nums[lk - 1], nums[rk - 1] };
-            }
-
-            // subproblem
-            var nselected = new int[n / 2 + 1];
-            for (int i = 0; i < n / 2; i++)
-                nselected[i] = selected[i * 2];
-            nselected[n / 2] = selected[n - 1];
-            int nlk = (lk + 3) / 4;
-            int nrk = n % 2 == 0 ? (rk + 4 * n + 3) / 4 + 1 : (rk + 2 * n) / 4 + 1;
-            var nr = Biselect(A, nselected, nlk, nrk);
-            double[] loValue = nr[0], hiValue = nr[1];
-
-            // calculate rank-(hi), rank+(lo) with saddleback binary search
-            int cntSmaller = 0, cntGreater = 0;
-            double[][][] listBetween = new double[n][][];
-            for (int js = n, jg = n, i = 0; i < n; i++)
-            {
-                while (js > 0 && MatrixItem(A, selected[i], selected[js - 1])[0] >= hiValue[0]) js--;
-                while (jg > 0 && MatrixItem(A, selected[i], selected[jg - 1])[0] > loValue[0]) jg--;
-                cntSmaller += js;
-                cntGreater += n - jg;
-                listBetween[i] = new double[Math.Max(0, js - jg)][];
-                for (int j = jg; j < js; j++)
-                    listBetween[i][j - jg] = MatrixItem(A, selected[i], selected[j]);
-            }
-
-            // compute result
-            bool hasMerged = false;
-            double[] lo = cntSmaller < lk ? hiValue
-                : lk + cntGreater <= n * n ? loValue
-                : Pick(listBetween, lk + cntGreater - n * n, ref hasMerged);
-            double[] hi = cntSmaller < rk ? hiValue
-                : rk + cntGreater <= n * n ? loValue
-                : Pick(listBetween, rk + cntGreater - n * n, ref hasMerged);
-
-            return new double[][] { lo, hi };
-        }
-
-        double[] Pick(double[][][] data, int k, ref bool hasMerged)
-        {
-            if (!hasMerged)
-            {
-                hasMerged = true;
-                Merge(data, 0, data.Length - 1);
-            }
-            return data[0][k - 1];
-        }
-
-        void Merge(double[][][] data, int lo, int hi)
-        {
-            if (lo >= hi) return;
-
-            int mid = (lo + hi) / 2;
-            Merge(data, lo, mid);
-            Merge(data, mid + 1, hi);
-
-            double[][] ls = data[lo], rs = data[mid + 1];
-            double[][] aux = new double[ls.Length + rs.Length][];
-            for (int i = 0, j = 0, k = 0; k < aux.Length; k++)
-            {
-                if (i < ls.Length && (j >= rs.Length || ls[i][0] < rs[j][0]))
-                    aux[k] = ls[i++];
-                else aux[k] = rs[j++];
-            }
-            data[lo] = aux;
-        }
-
-        double[] MatrixItem(int[] A, int row, int col)
-        {
-            return new double[] { (double)A[row] / A[A.Length - 1 - col], A[row], A[A.Length - 1 - col] };
         }
     }
 }
---
+++ codes/src/leetcode/500/Lc564_Find_the_Closest_Palindrome.cs
@@ -0,0 +1,162 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using alg;
+
+/*
+ * tags: array
+ * Time(n), Space(n)
+ */
+namespace leetcode
+{
+    public class Lc564_Find_the_Closest_Palindrome
+    {
+        public string NearestPalindromic(string n)
+        {
+            var pcs = n.ToCharArray();
+            for (int i = 0, j = pcs.Length - 1; i < j; i++, j--)
+                pcs[j] = pcs[i];
+            var ps = new string(pcs);
+            if (ps == n)
+            {
+                if (pcs.Length % 2 == 1)
+                {
+                    char c = pcs[pcs.Length / 2];
+                    pcs[pcs.Length / 2] = c == '0' ? '1' : (char)(c - 1);
+                }
+                else
+                {
+                    char c = pcs[pcs.Length / 2];
+                    pcs[pcs.Length / 2] = pcs[pcs.Length / 2 - 1] = c == '0' ? '1' : (char)(c - 1);
+                }
+                ps = new string(pcs);
+            }
+
+            char[] pcs2;
+            if (n[0] == '9')
+            {
+                pcs2 = new char[n.Length + 1];
+                Array.Fill(pcs2, '0');
+                pcs2[0] = pcs2[pcs2.Length - 1] = '1';
+            }
+            else
+            {
+                pcs2 = new char[n.Length];
+                Array.Fill(pcs2, '0');
+                pcs2[0] = pcs2[pcs2.Length - 1] = (char)(n[0] + 1);
+            }
+            var ps2 = new string(pcs2);
+
+            var pcs3 = new char[n.Length - 1];
+            Array.Fill(pcs3, '9');
+            var ps3 = new string(pcs3);
+            if (ps3 == "") ps3 = "99";
+
+            //var bn = BigInteger.Parse(n);
+            //var b = BigInteger.Parse(ps);
+            //var b2 = BigInteger.Parse(ps2);
+            //return BigInteger.Abs(b - bn) > BigInteger.Abs(b2 - bn) ? ps2 : ps;
+            string smallerps, smallerdiff;
+            var diff = Diff(ps, n);
+            var diff2 = Diff(ps2, n);
+            int c12 = CompareTo(diff, diff2);
+            if (c12 < 0 || (c12 == 0 && CompareTo(ps, ps2) < 0))
+            {
+                smallerdiff = diff;
+                smallerps = ps;
+            }
+            else
+            {
+                smallerdiff = diff2;
+                smallerps = ps2;
+            }
+
+
+            var diff3 = Diff(ps3, n);
+            int c3 = CompareTo(smallerdiff, diff3);
+            return c3 < 0 || (c3 == 0 && CompareTo(smallerps, ps3) < 0) ? smallerps : ps3;
+        }
+
+        string Diff(string a, string b)
+        {
+            string big = "", small = "";
+            if (a.Length != b.Length)
+            {
+                if (a.Length > b.Length)
+                {
+                    big = a;
+                    small = b;
+                }
+                else
+                {
+                    big = b;
+                    small = a;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] != b[i])
+                    {
+                        if (a[i] > b[i])
+                        {
+                            big = a;
+                            small = b;
+                        }
+                        else
+                        {
+                            big = b;
+                            small = a;
+                        }
+                        break;
+                    }
+                }
+            }
+
+            var diff = new char[big.Length];
+            int idx = diff.Length;
+            for (int carry = 0, i = big.Length - 1, j = small.Length - 1; i >= 0; i--, j--)
+            {
+                int val = big[i] - (j >= 0 ? small[j] : '0') - carry;
+                if (val < 0)
+                {
+                    val += 10;
+                    carry = 1;
+                }
+                else
+                {
+                    carry = 0;
+                }
+
+                diff[--idx] = (char)(val + '0');
+            }
+
+            while (idx < diff.Length && diff[idx] == '0') idx++;
+
+            return new string(diff, idx, diff.Length - idx);
+        }
+
+        int CompareTo(string a, string b)
+        {
+            if (a.Length != b.Length) return a.Length - b.Length;
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] != b[i]) return a[i] - b[i];
+            return 0;
+        }
+
+
+        public void Test()
+        {
+            Console.WriteLine(NearestPalindromic("1") == "0");
+            Console.WriteLine(NearestPalindromic("9") == "8");
+            Console.WriteLine(NearestPalindromic("10") == "9");
+            Console.WriteLine(NearestPalindromic("123") == "121");
+            Console.WriteLine(NearestPalindromic("1283") == "1331");
+            Console.WriteLine(NearestPalindromic("19999") == "20002");
+        }
+    }
+}
+
