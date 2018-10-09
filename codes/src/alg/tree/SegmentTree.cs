218df6416e707f142bf1c3e59e45f8f1b81bb9e2
unknown <meng789987@gmail.com>
2018-10-08 22:06:45 -0700

    Lc730_Count_Different_Palindromic_Subsequences: dp
     * Time(n^2), Space(n^2)
     * dp(i, j, c) = s[i] == c ? 1 : 0, if i == j,
     *               dp(i, j-1, c) if s[j] != c,
     *               dp(i+1, j, c) if s[i] != c,
     *               sum(dp(i+1, j-1, cc)) + 2, cc is any char
     * dp(i, j, c) is the number of total different palindromic
       subsequences ending with c of string s[i..j]

--- codes/src/alg/tree/SegmentTree.cs
+++ codes/src/alg/tree/SegmentTree.cs
@@ -5,19 +5,20 @@ using System.Linq;
 /*
  * tags: segment tree
  * Space(n); Build: Time(n), Query/Update: Time(logn)
- * Tree node has a sum/max/gcd/lcd for its data range. If a node is the sum of data range[i..j], 
- * then its left is sum of data range[i..(i+j)/2], and its right is sum of data range[(i+j)/2+1..j].
+ * Depending on its purpose, tree node stores the sum/max/gcd/lcd of its data range. If a node is the sum of data range[i..j], 
+ * then its left is sum of data range[i..m], where m=(i+j)/2, and its right is sum of data range[m+1..j].
  * e.g. for data [10, 13, 12, 15, 16, 18], it would be:
  *                84[0..5]
  *         35[0..2]            49[3..5]
  *   23[0..1]             31[3..4]    
  * 10[0]  13[1]  12[2]  15[3]  16[4]  18[5]
  * 
- * use an array, for node i its left is 2*i and right is 2*i+1. root is a[1]
- *                84[0..7]
- *          50[0..3]
+ * use an array, for node i its left is 2*i and right is 2*i+1. 
+ * leaf node[n+i]=data[i], while internal node[i] = node[2*i] + node[2*i+1].
+ *                84[0..5]
+ *                       50[2..5]
  *   23[0..1]      27[2..3]      34[4..5]
- * 10[0]  13[1]  12[2]  15[3]  16[4]  18[5]  0[6]  0[7]
+ * 10[0]  13[1]  12[2]  15[3]  16[4]  18[5]
  */
 namespace alg.tree
 {
@@ -28,18 +29,22 @@ namespace alg.tree
             public SegmentSumTree(int[] data)
             {
                 _data = data;
-                _root = Build(data, 0, data.Length - 1);
+                _root = Build(0, data.Length - 1);
             }
 
-            Node Build(int[] data, int lo, int hi)
+            Node Build(int lo, int hi)
             {
                 if (lo > hi) return null;
                 var node = new Node(lo, hi);
-                node.Sum = data[lo];
-                if (lo == hi) return node;
+                if (lo == hi)
+                {
+                    node.Sum = _data[lo];
+                    return node;
+                }
+
                 int m = (lo + hi) / 2;
-                node.Left = Build(data, lo, m);
-                node.Right = Build(data, m + 1, hi);
+                node.Left = Build(lo, m);
+                node.Right = Build(m + 1, hi);
                 node.Sum = node.Left.Sum + node.Right.Sum;
                 return node;
             }
@@ -53,7 +58,7 @@ namespace alg.tree
 
             void Update(Node node, int pos, int diff)
             {
-                if (node == null || pos < node.Min || node.Max < pos) return;
+                if (node == null || pos < node.Lo || node.Hi < pos) return;
                 node.Sum += diff;
                 Update(node.Left, pos, diff);
                 Update(node.Right, pos, diff);
@@ -67,8 +72,8 @@ namespace alg.tree
             // sum of data[lo..hi]
             int Query(Node node, int lo, int hi)
             {
-                if (node == null || hi < node.Min || node.Max < lo) return 0; // no overlap
-                if (lo <= node.Min && node.Max <= hi) return node.Sum;        // node is fully covered
+                if (node == null || hi < node.Lo || node.Hi < lo) return 0; // no overlap
+                if (lo <= node.Lo && node.Hi <= hi) return node.Sum;        // node is fully covered
                 return Query(node.Left, lo, hi) + Query(node.Right, lo, hi);  // node is partially covered
             }
 
@@ -77,13 +82,13 @@ namespace alg.tree
 
             class Node
             {
-                public int Min, Max;
+                public int Lo, Hi;
                 public int Sum;
                 public Node Left, Right;
-                public Node(int min, int max)
+                public Node(int lo, int hi)
                 {
-                    Min = min;
-                    Max = max;
+                    Lo = lo;
+                    Hi = hi;
                 }
             }
         }
@@ -92,12 +97,9 @@ namespace alg.tree
         {
             public SegmentSumTreeArray(int[] data)
             {
-                int n = 1;
-                while (n < data.Length) n <<= 1;
+                int n = data.Length;
                 _segtree = new int[2 * n];
-
-                for (int i = 0; i < data.Length; i++)
-                    _segtree[i + n] = data[i];
+                Array.Copy(data, 0, _segtree, n, n);
                 for (int i = n - 1; i > 0; i--)  // bottom up
                     _segtree[i] = _segtree[2 * i] + _segtree[2 * i + 1];
             }
@@ -114,26 +116,18 @@ namespace alg.tree
                 }
             }
 
-            // sum of data[i..j]
-            public int Query(int i, int j)
+            // sum of data[lo..hi]
+            public int Query(int lo, int hi)
             {
                 int sum = 0;
-                i += _segtree.Length / 2;
-                j += _segtree.Length / 2;
-                while (i <= j)
+                lo += _segtree.Length / 2;
+                hi += _segtree.Length / 2;
+                for (; lo <= hi; lo /= 2, hi/= 2)
                 {
-                    if (i % 2 == 1) // node i is right child, add itself instead of its parent
-                    {
-                        sum += _segtree[i];
-                        i++; // move to parent sibling
-                    }
-                    if (j % 2 == 0) // node j is left child, add itself instead of its parent
-                    {
-                        sum += _segtree[j];
-                        j--; // move to parent sibling
-                    }
-                    i /= 2;
-                    j /= 2;
+                    if (lo % 2 == 1) // node i is right child, add itself instead of its parent
+                        sum += _segtree[lo++]; // move to parent sibling
+                    if (hi % 2 == 0) // node j is left child, add itself instead of its parent
+                        sum += _segtree[hi--]; // move to parent sibling
                 }
                 return sum;
             }
--- codes/src/leetcode/700/Lc730_Count_Different_Palindromic_Subsequences.cs
+++ codes/src/leetcode/700/Lc730_Count_Different_Palindromic_Subsequences.cs
@@ -8,9 +8,10 @@ using alg;
 /*
  * tags: dp
  * Time(n^2), Space(n^2)
- * dp(i, j, c) = dp(i, j-1, c) if s[j] != c,
- *               1 if s[j] is first c in s[i..j],
- *               sum(dp(first(c)+1, j-1, cc)) + 2, cc is any char
+ * dp(i, j, c) = s[i] == c ? 1 : 0, if i == j,
+ *               dp(i, j-1, c) if s[j] != c,
+ *               dp(i+1, j, c) if s[i] != c,
+ *               sum(dp(i+1, j-1, cc)) + 2, cc is any char
  * dp(i, j, c) is the number of total different palindromic subsequences ending with c of string s[i..j]
  */
 namespace leetcode
@@ -18,6 +19,50 @@ namespace leetcode
     public class Lc730_Count_Different_Palindromic_Subsequences
     {
         public int CountPalindromicSubsequences(string s)
+        {
+            int n = s.Length;
+            var memo = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    memo[i, j] = -1;
+
+            return Count(s, 0, n - 1, memo);
+        }
+
+        int Count(string s, int lo, int hi, int[,] memo)
+        {
+            const int MOD = 1000000007;
+
+            if (lo > hi) return 0;
+            if (lo == hi) return memo[lo, hi] = 1;
+            if (memo[lo, hi] >= 0) return memo[lo, hi];
+
+            int ret = 0;
+            if (s[lo] != s[hi])
+            {
+                // all answers ending (any char in s[lo+1..hi-1] and s[hi])
+                // plus answers ending (any char in s[lo+1..hi-1] and s[lo])
+                // minus dup answers ending (any char in s[lo+1..hi-1])
+                ret = Count(s, lo + 1, hi, memo) + Count(s, lo, hi - 1, memo) - Count(s, lo + 1, hi - 1, memo);
+            }
+            else
+            {
+                int i = lo + 1, j = hi - 1;
+                while (i <= j && s[i] != s[lo]) i++;
+                while (i <= j && s[j] != s[lo]) j--;
+
+                if (i > j) // no s[lo] in substring s[lo+1..hi-1]
+                    ret = Count(s, lo + 1, hi - 1, memo) * 2 + 2;
+                else if (i == j)  // only one s[lo] in substring s[lo+1..hi-1]
+                    ret = Count(s, lo + 1, hi - 1, memo) * 2 + 1;
+                else // two or more s[lo] in substring s[lo+1..hi-1]
+                    ret = Count(s, lo + 1, hi - 1, memo) * 2 - Count(s, i + 1, j - 1, memo);
+            }
+
+            return memo[lo, hi] = (ret >= 0 ? ret : ret + MOD) % MOD;
+        }
+
+        public int CountPalindromicSubsequences2(string s)
         {
             const int MOD = 1000000007;
             int n = s.Length;
@@ -30,29 +75,27 @@ namespace leetcode
             int ret = 0;
             for (int c = 0; c < 4; c++)
             {
-                ret = (ret + Count(s, 0, n - 1, c, memo)) % MOD;
+                ret = (ret + Count2(s, 0, n - 1, c, memo)) % MOD;
             }
 
             return ret;
         }
 
-        int Count(string s, int lo, int hi, int c, int[,,] memo)
+        int Count2(string s, int lo, int hi, int c, int[,,] memo)
         {
             const int MOD = 1000000007;
             char ch = (char)(c + 'a');
 
+            if (lo > hi) return 0;
             if (memo[lo, hi, c] >= 0) return memo[lo, hi, c];
             if (lo == hi) return memo[lo, hi, c] = (s[lo] == ch ? 1 : 0);
-            if (s[hi] != ch) return memo[lo, hi, c] = Count(s, lo, hi - 1, c, memo);
-
-            int i = lo;
-            while (i < hi && s[i] != ch) i++;
-            if (i == hi) return memo[lo, hi, c] = 1;
+            if (s[lo] != ch) return memo[lo, hi, c] = Count2(s, lo + 1, hi, c, memo);
+            if (s[hi] != ch) return memo[lo, hi, c] = Count2(s, lo, hi - 1, c, memo);
 
             int ret = 0;
             for (int cc = 0; cc < 4; cc++)
             {
-                ret = (ret + Count(s, i, hi - 1, cc, memo)) % MOD;
+                ret = (ret + Count2(s, lo + 1, hi - 1, cc, memo)) % MOD;
             }
             ret = (ret + 2) % MOD;
 
