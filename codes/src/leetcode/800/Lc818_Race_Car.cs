dd272db6cd52bce700f519745dd2f9327925e4c6
meng789987 <meng789987@gmail.com>
2018-10-23 23:50:40 -0700

    Lc818_Race_Car: dp, back/forward

--- codes/src/leetcode/800/Lc818_Race_Car.cs
+++ codes/src/leetcode/800/Lc818_Race_Car.cs
@@ -5,6 +5,9 @@ using System.Linq;
 /*
  * tags: dp
  * Time(nlogn), Space(n)
+ * dp[t] = k, if t==2^k-1 (drive A^k);
+ *         min(dp[t], dp[t - 2^(k-1) + 2^j)] + k - 1 + j + 2), where j=[0..k-2] (drive to u then A^(k-1)RA^jR)
+ *         min(dp[t], dp[2^k - 1 - t] + k + 1) (drive A^kR then to t)
  */
 namespace leetcode
 {
@@ -12,23 +15,44 @@ namespace leetcode
     {
         public int Racecar(int target)
         {
-            if (_dp != null)
-                return _dp[target];
+            if (_dp[target] > 0) return _dp[target];
 
-            _dp = new int[10003];
+            int k = Convert.ToString(target, 2).Length;
+            if (target == (1 << k) - 1) return _dp[target] = k;
+
+            _dp[target] = Racecar((1 << k) - 1 - target) + k + 1;
+            for (int j = 0; j < k - 1; ++j)
+            {
+                var step = Racecar(target - (1 << (k - 1)) + (1 << j)) + k - 1 + j + 2;
+                _dp[target] = Math.Min(_dp[target], step);
+            }
+
+            return _dp[target];
+        }
+
+        public int Racecar2(int target)
+        {
+            if (_dp[target] > 0) return _dp[target];
+
             Array.Fill(_dp, int.MaxValue);
-            _dp[0] = 0; _dp[1] = 1; _dp[2] = 4;
+            _dp[0] = 0; _dp[1] = 1;
 
-            for (int t = 3; t < _dp.Length; ++t)
+            for (int t = 2; t < _dp.Length; t++)
             {
                 int k = Convert.ToString(t, 2).Length;
+
+                // case 1: exact 2^k-1. drive A^k
                 if (t == (1 << k) - 1)
                 {
                     _dp[t] = k;
                     continue;
                 }
+
+                // case 2: not cross 2^k-1. drive to u then A^(k-1)RA^jR
                 for (int j = 0; j < k - 1; ++j)
                     _dp[t] = Math.Min(_dp[t], _dp[t - (1 << (k - 1)) + (1 << j)] + k - 1 + j + 2);
+
+                // case 3: cross 2^k-1. drive A^kR then to t
                 if ((1 << k) - 1 - t < t)
                     _dp[t] = Math.Min(_dp[t], _dp[(1 << k) - 1 - t] + k + 1);
             }
@@ -36,7 +60,7 @@ namespace leetcode
             return _dp[target];
         }
 
-        static int[] _dp;
+        static int[] _dp = new int[10001];
 
         public void Test()
         {
