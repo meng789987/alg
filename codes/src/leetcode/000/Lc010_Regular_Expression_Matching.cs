aa555525fda1c076bba8791b874ea507d318a1ce
meng789987 <meng789987@gmail.com>
2019-04-24 17:29:33 -0700

    regular expression: greedy, dp(Edit Distance)

---
+++ codes/src/leetcode/000/Lc010_Regular_Expression_Matching.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using alg;
+
+/*
+ * tags: dp
+ * Time(mn), Space(n)
+ * dp[i, j] is true if s[0..i-1] matches p[0..j-1]
+ * dp[i, j] = dp[i-1, j-1], if s[i-1] match p[j-1] (s[i-1]==p[j-1] or p[j-1]=='.'), otherwise if p[j-1] is '*'
+ *          = dp[i, j-2], if s[i-1] not match p[j-2], "a*" match empty, otherwise s[i-1] match p[j-2]
+ *          = dp[i, j-2], "x*" match empty
+ *         or dp[i, j-1], "x*" match one
+ *         or dp[i-1, j], "x*" match two or more
+ *         
+ *  or similar "Edit Distance", just consider add and delete operations on pattern
+ */
+namespace leetcode
+{
+    public class Lc010_Regular_Expression_Matching
+    {
+        /*
+         * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+         *     '.' Matches any single character.
+         *     '*' Matches zero or more of the preceding element.
+        */
+        public bool IsMatch(string s, string p)
+        {
+            int m = s.Length, n = p.Length;
+            var dp = new bool[m + 1, n + 1];
+            dp[0, 0] = true;
+            for (int j = 2; j <= n && p[j - 1] == '*'; j += 2)
+                dp[0, j] = true;
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (s[i - 1] == p[j - 1] || p[j - 1] == '.')
+                        dp[i, j] = dp[i - 1, j - 1];
+                    else if (p[j - 1] == '*')
+                    {
+                        dp[i, j] = dp[i, j - 1] | dp[i, j - 2];  // "x*" match empty or one x, delete "x*"
+                        if (s[i - 1] == p[j - 2] || p[j - 2] == '.')
+                            dp[i, j] |= dp[i - 1, j]; // "x*" match more x, add more "x"
+                    }
+                }
+            }
+
+            return dp[m, n];
+        }
+
+        public void Test()
+        {
+            Console.WriteLine(IsMatch("aa", "a") == false);
+            Console.WriteLine(IsMatch("aa", "a*") == true);
+            Console.WriteLine(IsMatch("ab", ".*") == true);
+            Console.WriteLine(IsMatch("aab", "c*a*b") == true);
+            Console.WriteLine(IsMatch("mississippi", "mis*is*p*.") == false);
+            Console.WriteLine(IsMatch("mississippi", "mis*is*ip*.") == true);
+            Console.WriteLine(IsMatch("bbbba", ".*a*a") == true);
+        }
+    }
+}
+
--- codes/src/leetcode/000/Lc044_Wildcard_Matching.cs
+++ codes/src/leetcode/000/Lc044_Wildcard_Matching.cs
@@ -5,29 +5,83 @@ using System.Linq;
 using alg;
 
 /*
- * tags: regex
+ * tags: greedy, DP
  * Time(mn), Space(1)
- * Regular expression, support only '?' and '*' which matches 0-n any chars
+ * Greedy is tricky, start from empty match, if match failed restart from last '*' position
+ * 
+ * DP is similar with Edit Distance, '*' can be deleted, or add one char
+ * dp[i, j] is true when s[0..i-1] match p[0..j-1], then
+ * dp[i, j] = dp[i-1, j-1], if s[i-1]==p[j-1] or p[j-1]=='?'
+ *          = dp[i-1, j] | dp[i, j-1], if p[j-1]=='*',   // add one char, or delete '*'
  */
 namespace leetcode
 {
     public class Lc044_Wildcard_Matching
     {
-        public bool IsMatch(string s, string p)
+        // Regular expression, support only '?' and '*' which matches 0-n any chars
+        public bool IsMatch2(string s, string p)
         {
             int i = 0, j = 0, n = p.Length;
-            int si = -1, sj = -1; // index of last star '*'
+            int match = -1, star = -1; // index of last star '*'
 
             while (i < s.Length)
             {
                 if (j < n && (p[j] == '?' || p[j] == s[i])) { i++; j++; }
-                else if (j < n && p[j] == '*') { si = i; sj = j++; }
-                else if (si >= 0) { i = ++si; j = sj + 1; }
+                else if (j < n && p[j] == '*') { match = i; star = j++; }
+                else if (star >= 0) { i = ++match; j = star + 1; }
                 else return false;
             }
 
             while (j < n && p[j] == '*') j++;
             return j == n;
+        }
+
+        public bool IsMatchDp(string s, string p)
+        {
+            int m = s.Length, n = p.Length;
+            var dp = new bool[m + 1, n + 1];
+            dp[0, 0] = true;
+            for (int j = 1; j <= n && p[j - 1] == '*'; j++)
+                dp[0, j] = true;
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+                        dp[i, j] = dp[i - 1, j - 1];
+                    else if (p[j - 1] == '*')
+                        dp[i, j] = dp[i - 1, j] | dp[i, j - 1];
+                }
+            }
+
+            return dp[m, n];
+        }
+
+        public bool IsMatch(string s, string p)
+        {
+            int m = s.Length, n = p.Length;
+            var dp = new bool[n + 1];
+            dp[0] = true;
+            for (int j = 1; j <= n && p[j - 1] == '*'; j++)
+                dp[j] = true;
+
+            for (int i = 1; i <= m; i++)
+            {
+                var pre = dp[0];
+                dp[0] = false;
+                for (int j = 1; j <= n; j++)
+                {
+                    var tmp = dp[j];
+                    if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+                        dp[j] = pre;
+                    else
+                        dp[j] = (dp[j] | dp[j - 1]) && (p[j - 1] == '*');
+                    pre = tmp;
+                }
+            }
+
+            return dp[n];
         }
 
         public void Test()
