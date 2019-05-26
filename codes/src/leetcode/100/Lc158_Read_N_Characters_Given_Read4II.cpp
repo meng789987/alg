7190080020dadbcac883960cfec543acfa3af703
meng789987 <39682807+meng789987@users.noreply.github.com>
2019-05-25 22:03:45 -0700

    Create Lc158_Read_N_Characters_Given_Read4II.cpp

---
+++ codes/src/leetcode/100/Lc158_Read_N_Characters_Given_Read4II.cpp
@@ -0,0 +1,35 @@
+
+/*
+ * tags: design
+ * Time(n), Space(1)
+ */
+
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution {
+public:
+    Solution() : _cnt(0), _pos(0) {}
+    
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+        int r = 0;
+        while (r < n) {
+            if (_pos == 0) _cnt = read4(_buf);
+            if (_cnt == 0) break;
+            while (r < n && _pos < _cnt) 
+                buf[r++] = _buf[_pos++];
+            if (_pos == _cnt) _pos = 0;
+        }
+        
+        return r;
+    }
+    
+private:
+    char _buf[4];
+    int _cnt, _pos;
+};
